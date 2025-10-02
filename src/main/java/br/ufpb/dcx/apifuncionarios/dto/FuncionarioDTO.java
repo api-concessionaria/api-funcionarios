f7b57bb8b875eb6e769bccdf04ae363d7bbf9dbf
amaroelias <amaronascimento2016@hotmail.com>
2025-10-01 21:16:23 -0300

    ajustando dependencias e dto

--- src/main/java/br/ufpb/dcx/apifuncionarios/dto/FuncionarioDTO.java
+++ src/main/java/br/ufpb/dcx/apifuncionarios/dto/FuncionarioDTO.java
@@ -5,11 +5,13 @@ import br.ufpb.dcx.apifuncionarios.validation.Telefone;
 import jakarta.persistence.Column;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
 public class FuncionarioDTO {
     @NotBlank
     private String nome;
--- src/main/java/br/ufpb/dcx/apifuncionarios/model/Funcionario.java
+++ src/main/java/br/ufpb/dcx/apifuncionarios/model/Funcionario.java
@@ -1,6 +1,7 @@
 package br.ufpb.dcx.apifuncionarios.model;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -8,6 +9,7 @@ import lombok.NoArgsConstructor;
 @Table(name = "tbFuncionarios")
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
 public class Funcionario {
 
     @Id
